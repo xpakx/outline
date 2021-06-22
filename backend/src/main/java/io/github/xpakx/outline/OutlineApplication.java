98c50da588609104ee9c2a7b05f9a8fd194ba1f3
xpakx <55586470+xpakx@users.noreply.github.com>
2021-06-22 12:07:24 +0200

    chore: init backend

---
+++ backend/src/main/java/io/github/xpakx/outline/OutlineApplication.java
@@ -0,0 +1,13 @@
+package io.github.xpakx.outline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutlineApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/io/github/xpakx/outline/OutlineApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.xpakx.outline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
