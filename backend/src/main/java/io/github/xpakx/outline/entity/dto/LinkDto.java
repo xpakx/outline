a68109a4f612ef5f51e8ed0df032db4507dd4992
xpakx <55586470+xpakx@users.noreply.github.com>
2021-06-22 13:58:54 +0200

    fix: change dto to interface

--- backend/src/main/java/io/github/xpakx/outline/entity/dto/LinkDto.java
+++ backend/src/main/java/io/github/xpakx/outline/entity/dto/LinkDto.java
@@ -1,11 +1,6 @@
 package io.github.xpakx.outline.entity.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-public class LinkDto {
-    String longUrl;
-    String content;
+public interface LinkDto {
+    String getLongUrl();
+    String getContent();
 }
