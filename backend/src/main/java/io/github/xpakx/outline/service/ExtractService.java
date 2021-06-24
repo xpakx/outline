9b9688cb53e850a81b88b0e8a3024283ca574325
xpakx <55586470+xpakx@users.noreply.github.com>
2021-06-24 12:28:32 +0200

    chore: delete unnecessary imports

--- backend/src/main/java/io/github/xpakx/outline/service/ExtractService.java
+++ backend/src/main/java/io/github/xpakx/outline/service/ExtractService.java
@@ -3,12 +3,10 @@ package io.github.xpakx.outline.service;
 import org.springframework.stereotype.Service;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
-import javax.swing.text.html.Option;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
