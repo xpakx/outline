2823ca6e971d442201332e3426630a0e24659847
xpakx <55586470+xpakx@users.noreply.github.com>
2022-04-01 14:19:46 +0200

    chore(frontend): add component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StartComponent } from './component/start/start.component';
 import { DisplayComponent } from './component/display/display.component';
+import { LoadingComponent } from './component/loading/loading.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StartComponent,
-    DisplayComponent
+    DisplayComponent,
+    LoadingComponent
   ],
   imports: [
     BrowserModule,
