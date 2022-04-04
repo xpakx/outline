4524cffe7bdbc0517dccceb74d55875a8f9bc828
xpakx <55586470+xpakx@users.noreply.github.com>
2022-04-04 14:37:42 +0200

    feat(frontend/ui): animate progress bar on loading view

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DisplayComponent } from './component/display/display.component';
+import { LoadingComponent } from './component/loading/loading.component';
 import { StartComponent } from './component/start/start.component';
 
 const routes: Routes = [
   { path: '', component: StartComponent },
+  { path: 'loading', component: LoadingComponent },
   { path: ':shortUrl', component: DisplayComponent }
 ];
 
