0e99d652c26376dc4bf5e7de48d4bab290095ff5
xpakx <55586470+xpakx@users.noreply.github.com>
2022-04-05 16:34:12 +0200

    feat(frontend): add option to return to main view from 404 error view

--- frontend/src/app/component/not-found/not-found.component.ts
+++ frontend/src/app/component/not-found/not-found.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.css']
 })
-export class NotFoundComponent implements OnInit {
+export class NotFoundComponent {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit(): void {
+  back(): void {
+    this.router.navigate(['']);
   }
 
 }
